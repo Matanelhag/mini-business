e441dff3af04ffbbf39eec039dffeb3f45242fcb
Matanelhag <96495064+Matanelhag@users.noreply.github.com>
2026-02-10 21:31:34 +0200

    Disable service worker in service-worker.js
    Disable service worker for GitHub Pages deployment.

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,1 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
-});
+// service worker disabled for GitHub Pages
