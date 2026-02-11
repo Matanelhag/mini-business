3d00d323cd84a9586f4e9105cf541d60f4f340bb
Matanelhag <96495064+Matanelhag@users.noreply.github.com>
2026-02-11 14:18:45 +0200

    Create Firebase.js with configuration details
    Add Firebase configuration settings.

---
+++ Firebase.js
@@ -0,0 +1,7 @@
+// הגדרות Firebase שלך
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSender
